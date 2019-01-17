afd9a464e12472f8714ffb47ec7a22a3c5ee4b92
dosisod <39638017+dosisod@users.noreply.github.com>
2019-01-16 20:39:25 -0800

    cleaned up code, xpi's now signed

--- pwd.js
+++ pwd.js
@@ -1,8 +1,7 @@
-document.getElementById("clickr").addEventListener("click", function(event) {
-	site=document.getElementById("site").value
-	master=document.getElementById("master").value
-	len=document.getElementById("len").value
-
+document.getElementById("clickr").onclick=()=>{
+	//document.getElementById() wrapper
+	d=(e)=>{return document.getElementById(e).value}
+	
 	//the "+" and "\n" are there to be consistent with my pas.sh gist
-	document.getElementById("desc").innerHTML=btoa(sha512(site+"+"+master+"\n")).substring(0,len)
-}, false);
+	desc.innerText=btoa(sha512(d("site")+"+"+d("master")+"\n")).substring(0,d("len"))
+}
